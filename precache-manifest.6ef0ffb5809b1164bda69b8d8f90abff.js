a2613852b762e55105a3749695690726174a7aa6
Yaroslav <eragon2193@gmail.com>
2022-10-22 11:04:04 +0200

    Updates

---
+++ precache-manifest.6ef0ffb5809b1164bda69b8d8f90abff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cb76d8d2ed10dea7dfc",
+    "url": "/QuotingMachine/static/css/main.09ff2502.chunk.css"
+  },
+  {
+    "revision": "8cb76d8d2ed10dea7dfc",
+    "url": "/QuotingMachine/static/js/main.8cb76d8d.chunk.js"
+  },
+  {
+    "revision": "86554914986e5723be73",
+    "url": "/QuotingMachine/static/css/1.d1696ef6.chunk.css"
+  },
+  {
+    "revision": "86554914986e5723be73",
+    "url": "/QuotingMachine/static/js/1.86554914.chunk.js"
+  },
+  {
+    "revision": "979d16360f107584ad71",
+    "url": "/QuotingMachine/static/js/runtime~main.979d1636.js"
+  },
+  {
+    "revision": "6a78298df97bd90d6fc1f6b73fd836ed",
+    "url": "/QuotingMachine/static/media/twitter.6a78298d.svg"
+  },
+  {
+    "revision": "666e3db954870d7cdb27937e402272a1",
+    "url": "/QuotingMachine/index.html"
+  }
+];
